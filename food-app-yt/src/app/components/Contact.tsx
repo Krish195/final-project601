09275df9ea3a0ffe72f496af84d06cbeb4a02610
Mantavya Soni <mds33@njit.edu>
2023-12-16 00:32:12 -0500

    Updated upto the contact section

---
+++ food-app-yt/src/app/components/Contact.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+
+const Contact: React.FC = () => {
+  return (
+    <section id="contact" style={{ marginTop: '100px' }}>
+      <div style={{
+        flex: 1,
+        padding: '40px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <h2 style={{
+          fontSize: '24px',
+          color: '#333',
+          marginBottom: '20px',
+          fontWeight: 'bold'
+        }}>Contact Us</h2>
+        <p style={{
+          fontSize: '16px',
+          color: '#666',
+          marginBottom: '10px'
+        }}>123 Main StreetNew York, NY 10001</p>
+        <p style={{
+          fontSize: '16px',
+          color: '#666',
+          marginBottom: '10px'
+        }}>(123) 456-7890</p>
+        <p style={{
+          fontSize: '16px',
+          color: '#666',
+          marginBottom: '20px'
+        }}>info@luxuryrestaurant.com</p>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '24px',
+          color: '#333',
+          marginBottom: '10px'
+        }}>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
+            <FaFacebookF />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
+            <FaTwitter />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
+            <FaInstagram />
+          </a>
+          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
+            <FaLinkedinIn />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
+
+
--- food-app-yt/src/app/page.tsx
+++ food-app-yt/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Hero from './components/Hero'
 import Feature from "./components/Feature";
 import Testimonial from "./components/Testimonial";
+import CombinedComponent from './components/CombinedComponent';
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Feature />
       <Testimonial />
+      <CombinedComponent />
 
     </main>
   )
