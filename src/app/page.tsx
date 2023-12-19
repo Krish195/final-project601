938d40b896dbaeab9dc6261225db1665e9848bf8
Mantavya Soni <mds33@njit.edu>
2023-12-18 19:21:23 -0500

    A step back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Feature from "./components/Feature";
 import Testimonial from "./components/Testimonial";
 import CombinedComponent from './components/CombinedComponent';
 import Footer from './components/Footer';
-import GoogleAnalytics from './components/GoogleAnalytics';
-import CookieBanner from './components/CookieBanner'
+// import GoogleAnalytics from './components/GoogleAnalytics';
+// import CookieBanner from './components/cookiebanner'
 
 
 
@@ -14,14 +14,14 @@ import CookieBanner from './components/CookieBanner'
 export default function Home() {
   return (
     <>
-    <GoogleAnalytics GA_MEASUREMENT_ID="G-6DVKX14RW9" />
+    {/* <GoogleAnalytics GA_MEASUREMENT_ID="G-6DVKX14RW9" /> */}
       <main>
         <Hero />
         <Feature />
         <Testimonial />
         <CombinedComponent />
         <Footer />
-        <CookieBanner />
+        {/* <CookieBanner /> */}
 
       </main>
     </>
